5a1dd7c5ded5e6017bcaae580504ae1235813969
rajeshkr0405 <rajesh.kr.0405@gmail.com>
2024-12-18 23:31:41 +0530

    Nested Dictionaries

---
+++ NestedDictionaries.py
@@ -0,0 +1,11 @@
+# Nested Dictionaries
+Student = {
+    "name" : "Shradha",
+    "score" : {
+        "chem" : 98,
+        "phy" : 97,
+        "math" : 95,
+    }
+    
+}
+print(Student["score"])
