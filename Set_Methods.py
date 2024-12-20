4ce66a185bc8db95fff3181fb47d13a53c983fc9
rajeshkr0405 <40197096+rajeshkr0405@users.noreply.github.com>
2024-12-20 22:50:57 +0530

    Python fb 1 (#8)
    * completed for if elif and else conditions
    
    * Verify Odd even number
    
    * find the greatest numbers
    
    * List&tuples
    
    * ListSlicing
    
    * Listmethod
    
    * completed tuples
    
    * Dictionary In pyhton
    
    * Nested Dictionaries
    
    * Dictionary methods
    
    * Set in Python
    
    * set methods
    
    * p1
    
    * p2

---
+++ Set_Methods.py
@@ -0,0 +1,30 @@
+# set.add(el) #adds an element
+# set.remove(el) #removes the elem an
+# set.clear() #empties the set
+# set.pop() #remove the random value
+# sets ---->  mutable
+# set --> elements ---> immutable
+# collection = set()
+# collection.add(1)
+# collection.add(2)
+# collection.add(3)
+# collection.add(4)
+# collection.add("rajesh")
+# collection.add((1,2,3,4,5))
+# print(collection)
+# collection.remove(1)
+# #collection.clear()
+
+# print(collection.pop())
+
+
+#  Set Methods
+# --------- Set.union(set2) #cobines oth set values & return new
+# --------- set.intersection(set2) # combines common vlaues & return new
+# set1={1,2,3}
+# set2={2,3,4}
+# print(set1.union(set2))
+
+set1={1,2,3}
+set2={2,3,4}
+print(set1.intersection(set2))
---
+++ setinpython.py
@@ -0,0 +1,13 @@
+# set is the collection of the unordered items.
+# Each element in the set must be unique & immutable
+
+# nums = {1,2,3,4}
+# set2 = {1,2,2,2}
+# #rpeated elements stored only once , so it resolved to {1,2}
+# null_set = set() #empty set syntaax
+# collection = {1,2,3,4,4,4, "hello" , "world", "hello"} # it will exculde the duplicate value 
+# print(collection)
+# print(type(collection))
+# print(len(collection))
+collection = set() # empty set ; syntex
+print(type(collection))
