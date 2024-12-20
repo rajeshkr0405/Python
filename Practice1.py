279ab704c8d2e66172f39183eecbbf14f26e2e31
rajeshkr0405 <rajesh.kr.0405@gmail.com>
2024-12-20 22:28:42 +0530

    p1

---
+++ Practice1.py
@@ -0,0 +1,6 @@
+#Store following word meanings in a python dictionary
+dict = {
+     "cat" : "a small animal",
+     "table" : ["a piece of furniture","list of facts & figures"]
+}
+print(dict["cat"])
