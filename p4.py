b8ccb877c07393cd8efbb1f82cb6843ccb5b0dc2
rajeshkr0405 <40197096+rajeshkr0405@users.noreply.github.com>
2024-12-20 23:10:43 +0530

    Add files via upload

---
+++ p4.py
@@ -0,0 +1,9 @@
+#Figure out a way to store 9 & 9.0 as separate values in the set.
+# you can take help of built in data types
+# value = {9, 9.0}
+# print(value)
+values ={
+    ("float" , 9.0),
+    ("int" , 9)
+}
+print(values)
