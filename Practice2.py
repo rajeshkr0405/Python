f1ba65fc3ee14b1186047fedcee9a032e06134de
rajeshkr0405 <rajesh.kr.0405@gmail.com>
2024-12-20 22:37:41 +0530

    p2

---
+++ Practice2.py
@@ -0,0 +1,4 @@
+# You are given a list of subjects for student . Assume one classrom is required for 1 subject. How many class
+#are needed by all students
+suject = {"python","java","C++","javascript","java","python","java","C++","C"}
+print(len(suject))
