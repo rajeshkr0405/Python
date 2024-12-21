4b7ab38afc6cb2532d70859273025fbfbb4a4a6c
rajeshkr0405 <rajesh.kr.0405@gmail.com>
2024-12-21 16:25:22 +0530

    loop

--- loopsInpython.py
+++ loopsInpython.py
@@ -1,1 +1,11 @@
-print("Hello")
+# #Loops in python
+# #Loops are used to repeat insturctions
+# #two type sof loops 
+# #1st while
+# #for loop
+# while loops
+# while condition :
+#     #some work
+count =1
+while count<=5 :
+    print("Hello")
