fd7bb9461abd0c6c80bafad3b7620b827705615a
rajeshkr0405 <rajesh.kr.0405@gmail.com>
2024-12-25 19:26:21 +0530

    Loop

--- loopsInpython.py
+++ loopsInpython.py
@@ -6,6 +6,18 @@
 # while loops
 # while condition :
 #     #some work
-count =1
-while count<=5 :
-    print("Hello")
+# count =1
+# while count<=5 :
+#     print("Hello")
+#     count +=1
+#     print("Out of loop")
+#     print(count)
+#print number from 1 to 5
+# i=1
+# while i<=5:
+#     print(i)
+#     i +=1
+i=5
+while i>=1:
+    print(i)
+i -=1
