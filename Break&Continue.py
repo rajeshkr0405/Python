d3f6f8230bf556dff99f6137a00185857c4f2cf0
rajeshkr0405 <rajesh.kr.0405@gmail.com>
2025-01-09 23:02:37 +0530

    added new

---
+++ Break&Continue.py
@@ -0,0 +1,3 @@
+#Break & Continue 
+#Break : Used to terminate the loop when encountered.
+#Continue : terminate execution in the current iteration & continue execution of the loop whith the iteratio
