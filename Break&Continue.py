a5a060e2cde7ea6740c529dd4618a7605ff35868
rajeshkr0405 <rajesh.kr.0405@gmail.com>
2025-01-11 20:36:42 +0530

    Break

--- Break&Continue.py
+++ Break&Continue.py
@@ -1,3 +1,10 @@
 #Break & Continue 
 #Break : Used to terminate the loop when encountered.
 #Continue : terminate execution in the current iteration & continue execution of the loop whith the iteratio
+i =1
+while i<= 10 :
+    if(i%2 != 0):
+        i +=1
+        continue
+    print(i)
+    i +=1
