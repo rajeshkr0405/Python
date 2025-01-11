ac440d5ce1472427131c1fae29f3589d8a6305fb
rajeshkr0405 <rajesh.kr.0405@gmail.com>
2025-01-11 21:20:22 +0530

    range

---
+++ rangeFunction.py
@@ -0,0 +1,20 @@
+"""
+Range function returns a sequence of numers, starting from 0 by default , and incremnets by 
+1( by default ), and stops efore a speficied number.
+range (start?, stop, step?)
+for el in range(5);
+print(el)
+for el in range(1,5):
+print(el)
+for el in range(1,5,2):
+print(el)
+"""
+"""seq = range(5)
+for i in seq:
+    print(i)"""
+
+"""for i in range(2,10): #range (start,stop)
+    print(i)"""
+
+"""for i in range(2,10,2): #range (start, stop, step)
+    print(i)"""
