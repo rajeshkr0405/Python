ebd8ea721c560ceb15712d9f3caa3ad8c96b2121
rajeshkr0405 <rajesh.kr.0405@gmail.com>
2025-01-20 21:48:39 +0530

    passa

---
+++ pass.py
@@ -0,0 +1,9 @@
+#Pass Statment
+#Pass is a null statment that does nothing . is is used as a placeholder for future code.
+"""
+for el in range(10):
+pass
+"""
+"""for i in range(5):
+    pass
+    print("Use full work")"""
