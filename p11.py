92a11cde939dfc36847acdd7d2cd3a06b2e84563
rajeshkr0405 <40197096+rajeshkr0405@users.noreply.github.com>
2025-01-20 22:06:52 +0530

    funtion

---
+++ p11.py
@@ -0,0 +1,8 @@
+#WAP to print the lenght of a list.( list is the parameter)
+
+cities = ["Delhi", "Gurgaon", "Noida", "Chennai", "Mumbai"]
+heroes = ["Thor", "Catain of america", "Shaktiman"]
+def print_len(list):
+    print(list)
+print_len(cities)
+print_len(heroes)
